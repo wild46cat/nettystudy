6e919c4111b7ba77b9439c44a0232d2246450fc9
wildcat <409898859@qq.com>
2018-09-02 11:20:18 +0800

    add log

--- src/main/java/com/xueyou/demo/App.java
+++ src/main/java/com/xueyou/demo/App.java
@@ -1,10 +1,21 @@
 package com.xueyou.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  */
 public class App {
+
+    static final Logger LOG = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
+        LOG.trace("Hello World!");
+        LOG.debug("How are you today?");
+        LOG.info("I am fine.");
+        LOG.warn("I love programming.");
+        LOG.error("I am programming.");
         System.out.println("hello world");
     }
 }
--- src/main/java/com/xueyou/demo/server/HttpServer.java
+++ src/main/java/com/xueyou/demo/server/HttpServer.java
@@ -5,18 +5,13 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
-import io.netty.channel.nio.NioEventLoop;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.codec.LineBasedFrameDecoder;
 import io.netty.handler.codec.string.StringDecoder;
 
-/**
- * Created by wuxueyou on 2018/7/5.
- */
-public class TimerServer {
-
+public class HttpServer {
     public void bind(int port) throws Exception {
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
@@ -39,18 +34,19 @@ public class TimerServer {
     private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
         @Override
         protected void initChannel(SocketChannel socketChannel) throws Exception {
-
+            socketChannel.pipeline().addLast(new LineBasedFrameDecoder(1024));
+            socketChannel.pipeline().addLast(new StringDecoder());
+            socketChannel.pipeline().addLast(new TimeServerHandler());
         }
     }
 
     public static void main(String[] args) {
         int port = 56789;
         try {
-            new TimerServer().bind(port);
+            new HttpServer().bind(port);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
         }
     }
 }
-
